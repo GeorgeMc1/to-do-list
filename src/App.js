55095814e6dbae80fbb1bc97a3335665de0f0b48
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-24 14:10:29 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import { DndProvider } from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend"
 import './App.css';
+import ListItem from "./components/ListItem";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState(["test"]);
+  const getInput = (e) => {
+    setInput(e.target.value);
+  }
+  const addItem = () => {
+    if (input !== ""){
+      let clone = [...list];
+      clone.push(input);
+      setList(clone);
+      setInput("");
+    }
+  }
+  const removeItem = (item) => {
+    let clone = [...list];
+    clone.splice(item, 1);
+    setList(clone);
+  }
+  const enterKey = (e) => {
+    if (e.key === "Enter"){
+      addItem();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DndProvider backend={HTML5Backend}>
+      <h1>To Do List</h1>
+      <div id="inputBox">
+        <input id="input" onChange={getInput} onKeyDown={enterKey} value={input}></input>
+        <button onClick={addItem}>+</button>
+      </div>
+      <div id="items">
+        {list.map((item, i) => {
+          return <ListItem key={i} label={item} click={() => removeItem(i)} enter={enterKey}/>
+        })}
+      </div>
+      </DndProvider>
     </div>
   );
 }
